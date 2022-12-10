1a7870248617845622fdbc1c2b2398da46a4fa94
Name <email@example.ru>
2022-12-10 16:34:31 +0500

    animation of tubes fixed

--- src/entities/tube.js
+++ src/entities/tube.js
@@ -3,75 +3,56 @@ import Entity from "./base.js";
 export default class Tube extends Entity {
     constructor(params) {
         super (params)
-        this._index = 0
-        this.xUp = this.x + this.width + 20
+        this.xUp = this.width + 20
         this.yUp = 0
     }
 
     draw() {
-        super.draw()
         //отрисовываем нижнюю трубу
-        // if (this._frameIndex === 0) {
-        //     this._drawSource.drawImage({
-        //         spriteSheet: this._spriteSheet, 
-        //         image: this._frames[this._frameIndex], 
-        //         x: this.x, 
-        //         y: this.y, 
-        //         width: this.width, 
-        //         height: this.height
-            
-        //         })
-        // } 
-        //if ((this._frameIndex === 1)) {
-            //отрисовываем верхнюю трубу
-            this._drawSource.drawImage({
-                spriteSheet: this._spriteSheet, 
-                image: this._frames[this._frameIndex], 
-                x: this.x + this.width * 5, 
-                y: this.y, 
-                width: this.width, 
-                height: this.height
-                })
+        super.draw()
+         //отрисовываем след. нижнюю трубу
+        this._drawSource.drawImage({
+            spriteSheet: this._spriteSheet, 
+            image: this._frames[this._frameIndex], 
+            x: this.x + this.width * 5, 
+            y: this.y, 
+            width: this.width, 
+            height: this.height
+            })
+        // аналогично рисуем верхие трубы
+        this._drawSource.drawImage({
+            spriteSheet: this._spriteSheet, 
+            image: this._frames[this._frameIndex+1], 
+            x: this.xUp, 
+            y: this.yUp, 
+            width: this.width, 
+            height: this.height
+            })
 
-            this._drawSource.drawImage({
-                spriteSheet: this._spriteSheet, 
-                image: this._frames[this._frameIndex+1], 
-                x: this.xUp, 
-                y: this.yUp, 
-                width: this.width, 
-                height: this.height
-                })
-
-            this._drawSource.drawImage({
-                spriteSheet: this._spriteSheet, 
-                image: this._frames[this._frameIndex+1], 
-                x: this.xUp + this.width * 3, 
-                y: this.yUp, 
-                width: this.width, 
-                height: this.height
-                })
+        this._drawSource.drawImage({
+            spriteSheet: this._spriteSheet, 
+            image: this._frames[this._frameIndex+1], 
+            x: this.xUp + this.width * 6, 
+            y: this.yUp, 
+            width: this.width, 
+            height: this.height
+            })
+           
         //}
 
     }
 
     update(delta) { 
-        this._index++;
-        if (this._index > this.width*7) {
-            this._index = 0;
-            this.x = + this.width * 5
+        // изменяем X для бесконечного появления труб
+        if (this.x < -this.width) {
+            this.x = this.width * 4
         }
-        // if (this._frameIndex === 0) {
-        //     this._frameIndex++
-        // } else {
-        //     this._frameIndex-- 
-        // }
 
-        //this._index += 0.3;
-        //this.x -= this._index * this.animationSpeed
-        //if (this._frameIndex > 0) {
-            
-        //}
+        if (this.xUp < -this.width) {
+            this.xUp = this.width * 5
+        }
 
+        //плавно изменяем X для эффекта движения
         this.x -= Math.ceil(delta * this.animationSpeed);
         this.xUp -= Math.ceil(delta * this.animationSpeed);
         
