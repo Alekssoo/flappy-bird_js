01d364c470338d722066b0f5593e9b384cbf45e2
Name <email@example.ru>
2022-12-25 22:52:53 +0500

    deleted unnecassary comments

--- src/entities/bird.js
+++ src/entities/bird.js
@@ -16,21 +16,6 @@ export default class Bird extends Entity {
         if (this.y < 0) {
             this.y = 0 // при столкновении с потолком возвращаем птичку вниз
         }
-
-        // if (this._game.tube.tubes[this._game.tube.index-2] && this._game.tube.tubesUp[this._game.tube.index-2]) {
-        //     if ((this.this.y + this.height >= this._game.height) // при достижении земли
-        //     //общее условие столкновения с любой трубой
-        //         || ((this.this.x + this.width >= this._game.tube.tubes[this._game.tube.index-2].x)
-        //     && (this.x < this._game.tube.tubes[this._game.tube.index-2].x + this._game.tube.width)
-        //     //столкновение с нижней трубой
-        //         && (((this.this.y + this.height > this._game.tube.tubes[this._game.tube.index-2].y)
-        //         && (this.y < this._game.tube.tubes[this._game.tube.index-2].y + this._game.tube.tubes[this._game.tube.index-2].height))
-        //     // или с верхней трубой 
-        //         || (this.y < this._game.tube.tubesUp[this._game.tube.index-2].height + this._game.tube.tubesUp[this._game.tube.index-2].y)))
-        //     ) {
-        //     this._game.defeat();
-        //     }
-        // }
     }
 
     flap() {
