c83df3ef93b2c7f049cac469e4392aad0dd6b106
Name <email@example.ru>
2022-12-28 21:18:26 +0500

    some small fixes

--- src/audioSources.js
+++ src/audioSources.js
@@ -10,8 +10,6 @@ export default class WebApiAudioSource extends AudioSource {
     constructor() {
         super()
         
-        //this._context = getAudioContext() //new AudioContext()
-        //this._audio
     }
 
     getAudioContext() {
@@ -35,23 +33,9 @@ export default class WebApiAudioSource extends AudioSource {
                 this._gainNode = this._context.createGain();
                 // создаем источник буфера
                 this._source = this._context.createBufferSource()
-                // устанавливаем буфер и соединяем с затуханием
-                // this._source.buffer = this._audio
-                // this._source.connect(this._gainNode)
-                // this._gainNode.connect(this._context.destination)
     }
 
     play() {
-        // // получаем проверенный путь к файлу с аудио из запроса и декодируем
-        // this.getAudioContext();
-        // src.then(
-        //     (source) => {
-        //         console.log("source = ", source)
-        //         fetch(source)
-        //         .then(data => data.arrayBuffer())
-        //         .then(arrayBuffer => this._context.decodeAudioData(arrayBuffer))
-        //         .then(decodedAudio => {this._audio = decodedAudio})
-        //     })
 
         this._gainNode = this._context.createGain();
         // создаем источник буфера
